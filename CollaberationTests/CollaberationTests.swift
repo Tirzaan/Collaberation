c94a21aec2b89ac908f77a5df6b7d865b40ca060
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-15 10:09:01 -0500

    Initial Commit

---
+++ CollaberationTests/CollaberationTests.swift
@@ -0,0 +1,17 @@
+//
+//  CollaberationTests.swift
+//  CollaberationTests
+//
+//  Created by Tirzaan on 7/15/25.
+//
+
+import Testing
+@testable import Collaberation
+
+struct CollaberationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
